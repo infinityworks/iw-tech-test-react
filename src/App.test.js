ca8c00cb5a1ddd27b3987528cd42989858ac869e
Dom Henjes <dom.henjes@infinityworks.com>
2021-12-22 11:17:57 +0000

    Update tests & package.json - use --openssl-legacy-provider flag so there are no issues running 'out of the box' with newest React version - add jest config to package.json - remove vulnerabilities from older packages - expand on App.test.js to use act() and mock the API call

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { act } from 'react-dom/test-utils';
 
-it("renders without crashing", () => {
+it("renders without crashing", async () => {
+  const mockResponse = { "establishments": [] };
+  fetch.mockResponseOnce(JSON.stringify(mockResponse));
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  await act(async () => {
+    ReactDOM.render(<App />, div);
+  });
+  expect(div.querySelector('h2').innerHTML).toBe('Food Hygiene Ratings');
   ReactDOM.unmountComponentAtNode(div);
 });
