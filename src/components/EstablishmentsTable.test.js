199bdc3597e0af7d773e56e5946412a39e175744
Oliver Parkinson <olliepar01@gmail.com>
2022-01-13 11:05:27 +0000

    Fixed a linting issue

--- src/components/EstablishmentsTable.test.js
+++ src/components/EstablishmentsTable.test.js
@@ -7,26 +7,26 @@ import { EstablishmentsTable } from "./EstablishmentsTable";
 jest.mock("swr", () => jest.fn());
 
 describe("<EstablishmentsTable/>", () => {
-    it("renders", () => {
-        useSWR.mockImplementation(() => ({
-            data: {
-                establishments: [
-                    {
-                        BusinessName: "Test Business Name",
-                        RatingValue: "Test Rating Value",
-                    },
-                ],
-            },
-            error: null,
-        }));
+  it("renders", () => {
+    useSWR.mockImplementation(() => ({
+      data: {
+        establishments: [
+          {
+            BusinessName: "Test Business Name",
+            RatingValue: "Test Rating Value",
+          },
+        ],
+      },
+      error: null,
+    }));
 
-        const { getByText } = render(<EstablishmentsTable pageNumber={1} />);
+    const { getByText } = render(<EstablishmentsTable pageNumber={1} />);
 
-        expect(useSWR).toHaveBeenCalledWith(
-            "https://api.ratings.food.gov.uk/Establishments/basic/1/10",
-            fetcher
-        );
-        expect(getByText("Business Name")).toBeInTheDocument();
-        expect(getByText("Rating Value")).toBeInTheDocument();
-    })
-})
+    expect(useSWR).toHaveBeenCalledWith(
+      "https://api.ratings.food.gov.uk/Establishments/basic/1/10",
+      fetcher
+    );
+    expect(getByText("Business Name")).toBeInTheDocument();
+    expect(getByText("Rating Value")).toBeInTheDocument();
+  });
+});
